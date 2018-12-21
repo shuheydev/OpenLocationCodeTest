5aff1dc0e36c85e447312f3963ba0cd4b6d21e01
shuhey <shuhey.dev@gmail.com>
2018-12-22 01:40:10 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenLocationCodeTest/PlusCodeResponse.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace OpenLocationCodeTest
+{
+    //Json貼り付け時にクラスを生成
+
+    [DataContract]
+    public class PlusCodeResponse
+    {
+        [DataMember(Name = "plus_code")]
+        public Plus_Code PlusCode { get; set; }
+        [DataMember(Name = "status")]
+        public string status { get; set; }
+    }
+
+    [DataContract]
+    public class Plus_Code
+    {
+        [DataMember(Name = "global_code")]
+        public string GlobalCode { get; set; }
+        [DataMember(Name = "geometry")]
+        public Geometry Geometry { get; set; }
+        [DataMember(Name = "local_code")]
+        public string LocalCode { get; set; }
+        [DataMember(Name = "locality")]
+        public Locality Locality { get; set; }
+        [DataMember(Name = "best_street_address")]
+        public string BestStreetAddress { get; set; }
+    }
+
+    [DataContract]
+    public class Geometry
+    {
+        [DataMember(Name = "bounds")]
+        public Bounds Bounds { get; set; }
+        [DataMember(Name = "location")]
+        public Location Location { get; set; }
+    }
+
+    [DataContract]
+    public class Bounds
+    {
+        [DataMember(Name = "northeast")]
+        public Northeast northeast { get; set; }
+        [DataMember(Name = "southwest")]
+        public Southwest southwest { get; set; }
+    }
+
+    [DataContract]
+    public class Northeast
+    {
+        [DataMember(Name = "lat")]
+        public float Lat { get; set; }
+        [DataMember(Name = "lng")]
+        public float Lng { get; set; }
+    }
+
+    [DataContract]
+    public class Southwest
+    {
+        [DataMember(Name = "lat")]
+        public double Lat { get; set; }
+        [DataMember(Name = "lng")]
+        public double Lng { get; set; }
+    }
+
+    [DataContract]
+    public class Location
+    {
+        [DataMember(Name = "lat")]
+        public double Lat { get; set; }
+        [DataMember(Name = "lng")]
+        public double Lng { get; set; }
+    }
+
+    [DataContract]
+    public class Locality
+    {
+        [DataMember(Name = "local_address")]
+        public string local_address { get; set; }
+    }
+
+
+}
---
+++ OpenLocationCodeTest/Program.cs
@@ -0,0 +1,107 @@
+﻿using Google.OpenLocationCode;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+
+
+namespace OpenLocationCodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //global codes, such as "796RWF8Q+WF"
+            //local codes, such as "WF8Q+WF"
+            //local codes with a locality, such as "WF8Q+WF Praia, Cabo Verde"
+
+
+            //グローバルコードはそのままいける
+            var locationCode = "8Q9WCVW2+JFJ";
+            var olc = new OpenLocationCode(locationCode);//グローバルコードを入れれば、その経緯度をローカルで計算できる。
+            var decoded = olc.Decode();
+
+            Console.WriteLine($"GlobalCode:::lat:{decoded.CenterLatitude}, lon:{decoded.CenterLongitude}");
+
+            //ローカルコードは基準となる経緯度をもとに、計算する。
+            locationCode = "CVW2+JFJ";
+            olc = new OpenLocationCode(locationCode);
+
+            //基準となる経緯度はローカルコードが指す場所の比較的近くである必要がある。
+            //37.4614948,-23.5037148
+            //デバイスのGPS情報を使えば、自分の周囲の位置を指す経緯度を計算することができる。
+            double lat = 14.9218848;//緯度:南北
+            double lon = 138.7775175;//経度：東西
+            var recoverdOlc = olc.Recover(lat, lon);//基準となる経緯度を指定してグルーバルコードのOpenLocationCodeオブジェクトを取得する。
+            decoded = recoverdOlc.Decode();
+
+            Console.WriteLine($"LocalCode:::lat:{decoded.CenterLatitude}, lon:{decoded.CenterLongitude}");
+
+
+            //ショートコード＋都市（場所名）は処理できない。
+            //Google Map ApiというかGoogle Geocoode apiで経緯度を求める必要がある。
+            try
+            {
+                locationCode = "CVW2+JFJ Niigata,Nagaoka";
+                olc = new OpenLocationCode(locationCode); //これはできない。
+                decoded = olc.Decode();
+
+                Console.WriteLine($"LocalCodeWithCity:::lat:{decoded.CenterLatitude}, lon:{decoded.CenterLongitude}");
+            }
+            catch
+            {
+                //例外を握りつぶす
+            }
+
+
+
+            //GETメソッドで
+            using (var httpClient = new HttpClient())
+            {
+                var uri = $"https://plus.codes/api?address=WF8Q%2BWF%20Praia,%20Cabo%20Verde&ekey=＜あなたのキー＞";
+
+                var responseString = httpClient.GetAsync(uri).Result.Content.ReadAsStringAsync().Result;
+
+                Console.WriteLine(responseString);
+            }
+
+            //POSTメソッドで
+            using (var httpClient = new HttpClient())
+            {
+                var httpRequest = new FormUrlEncodedContent(
+                    new Dictionary<string, string>()
+                    {
+                        {"address","WF8Q+WF Praia, Cabo Verde" },
+                        {"ekey","＜あなたのキー＞" },
+                    }
+                );
+
+                var httpResponse = httpClient.PostAsync("https://plus.codes/api", httpRequest).Result;
+
+                //Jsonをデシリアライズ
+                var response = httpResponse.Content.ReadAsStreamAsync().Result;
+
+                var jsonSerializer = new DataContractJsonSerializer(typeof(PlusCodeResponse));
+                var responseJson = jsonSerializer.ReadObject(response) as PlusCodeResponse;
+
+
+                lat = responseJson.PlusCode.Geometry.Location.Lat;
+                lon = responseJson.PlusCode.Geometry.Location.Lng;
+
+
+                Console.WriteLine($"Response:::lat:{lat}, lon:{lon}");
+
+
+                //Jsonをそのまま出力
+                var responseString = httpResponse.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseString);
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
